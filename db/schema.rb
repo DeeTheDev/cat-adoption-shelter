45c9a362120559778cfb0d189e6370cd46d47018
Denilson V <deethedev@gmail.com>
2021-01-18 12:20:57 -0500

    Removed password_confirmation from User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_165901) do
+ActiveRecord::Schema.define(version: 2021_01_18_171849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2021_01_18_165901) do
     t.string "last_name"
     t.string "email"
     t.string "password"
-    t.string "password_confirmation"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "encrypted_password", default: "", null: false
