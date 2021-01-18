4fdf7a016a3d6dfe2e77cf3e45fe6d6661bbfddc
Denilson V <deethedev@gmail.com>
2021-01-18 12:11:37 -0500

    Devise installation and setup/ Added pages controller

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #config action mailer Default Url (step for Devise setup)
+  #Note: Production may need setup when deployed*
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
