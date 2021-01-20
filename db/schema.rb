8b9b7d07398a585296d5bd5fc6da5d56f19c2624
Denilson V <deethedev@gmail.com>
2021-01-20 15:30:22 -0500

    Removed Password Column From Users DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_171849) do
+ActiveRecord::Schema.define(version: 2021_01_20_200238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2021_01_18_171849) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "encrypted_password", default: "", null: false
