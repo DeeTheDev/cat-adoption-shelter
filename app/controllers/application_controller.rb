c059b6e445642a7d0d15d342b4be825013696538
Denilson V <deethedev@gmail.com>
2021-01-20 15:29:08 -0500

    Configured Devise & Setup Custom Params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+  # #Adds permitted params when user sign_up (ex: first_name)
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    # devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password)}
+  end
 end
