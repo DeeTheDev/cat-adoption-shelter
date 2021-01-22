d55c2ffd2fb4d3b2baad53689b11d60d435446ac
Denilson V <deethedev@gmail.com>
2021-01-22 11:39:11 -0500

    Allowed Remember_me param in User Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -27,7 +27,7 @@ class SessionsController < Devise::SessionsController
     end
 
     def user_params
-        params.require(:user).permit(:email, :password)
+        params.require(:user).permit(:email, :password, :remember_me)
     end
 
 end
