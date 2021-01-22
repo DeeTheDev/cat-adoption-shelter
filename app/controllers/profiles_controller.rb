78f6fdb7406ccb5a1f725fdce7225185abbf0f05
Denilson V <deethedev@gmail.com>
2021-01-21 20:55:27 -0500

    Added Edit and Update methods to ProfilesController

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,28 @@
+class ProfilesController < ApplicationController
+    before_action :set_profile, only: [:edit, :update]
+
+    def edit
+        @cat = Cat.find(params[:cat_id])
+        @profile = Profile.find_by(user_id: current_user.id)
+    end
+
+    def update
+        if @profile.update(profile_params)
+            flash.notice = "The profile was updated successfully."
+            redirect_to cats_path
+        else
+            flash.now.alert = @profile.errors.full_messages.to_sentence
+            render :edit
+        end
+    end
+
+    private
+
+    def set_profile
+        @profile = Profile.find_by(user_id: current_user.id)
+    end
+    
+    def profile_params
+        params.require(:profile).permit(:user_id, :street, :street_2, :city, :state, :zip_code, :tel_number, :mobile_number, :of_age)
+    end
+end
