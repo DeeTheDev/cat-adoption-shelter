f2f077d9201cd47fb932fbb65f395b4c29169b13
Denilson V <deethedev@gmail.com>
2021-01-22 11:30:11 -0500

    Updated routes to reflex new resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,21 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions' }
   root to: 'pages#home'
 
+  resources :pages
+  resources :dashboards
+  get '/dashboards/show/:cat_id', to: 'dashboards#show'
+
+
   resources :cats
   get '/cats/adopt/:id', to: 'cats#adopt'
-  get '/cats/pickup/:id', to: 'cats#pickup'
+
+  resources :appointments, only: [:new, :create]
+  get '/appointment/new/:cat_id', to: 'appointments#new'
+  post '/appointment/:cat_id', to: 'appointments#create'
+
+  resources :deliveries, only: [:new, :create]
+  get '/delivery/new/:cat_id', to: 'deliveries#new'
+  post '/delivery/:cat_id', to: 'deliveries#create'
 
   resources :profiles, only: [:edit, :update]
   get '/profiles/edit/:cat_id', to: 'profiles#edit' 
