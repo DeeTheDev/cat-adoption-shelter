147161cf2fb84a965c167fce886e748aca387fff
Denilson V <deethedev@gmail.com>
2021-01-21 20:59:46 -0500

    Updated routes to reflect Controllers methods

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :cats
+  get '/cats/adopt/:id', to: 'cats#adopt'
+  get '/cats/pickup/:id', to: 'cats#pickup'
 
+  resources :profiles, only: [:edit, :update]
+  get '/profiles/edit/:cat_id', to: 'profiles#edit' 
+  put '/profiles/:cat_id/', to: 'profiles#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
