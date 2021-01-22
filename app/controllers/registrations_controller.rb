c064c621ec2234c0ec7d93a79aa21f14908d9e5f
Denilson V <deethedev@gmail.com>
2021-01-21 20:59:24 -0500

    Configured RegistrationController #create Method to Create Profile Table for New Users

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,8 @@ class RegistrationsController < Devise::RegistrationsController
     def create
         @user = User.new(user_params)
         if @user.save
-        render json: @user
+            @user.create_profile!
+            render json: @user
         else
         warden.custom_failure!
         render json: { error: 'signup error' }, status: :unprocessable_entity
