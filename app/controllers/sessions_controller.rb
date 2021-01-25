6fb1945c866dab83c66b5a167b4ee1827c9b2692
Denilson V <deethedev@gmail.com>
2021-01-25 08:59:56 -0500

    Updated Devise Sessions Controller redirect_to links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < Devise::SessionsController
 
-    # POST /v1/login
     def create
         @user = User.find_by_email(user_params[:email])
         return invalid_login_attempt unless @user
 
         if @user.valid_password?(user_params[:password])
         sign_in :user, @user
-        render json: @user
+        flash.notice = "Welcome back to our Cat Adoption Shelter!"
+        redirect_to pages_path
         else
         invalid_login_attempt
         end
@@ -15,15 +15,16 @@ class SessionsController < Devise::SessionsController
 
     def destroy
         sign_out(@user)
-        render :json=> {:success=>true}
+        flash.notice = "You have logged out successfully!"
+        redirect_to pages_path
     end
 
 
     private
 
     def invalid_login_attempt
-        warden.custom_failure!
-        render json: {error: 'invalid login attempt'}, status: :unprocessable_entity
+        flash.notice = "Invalid credentials, please try again."
+        redirect_to new_user_session_path
     end
 
     def user_params
